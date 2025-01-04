19134a124ae13646a2cb9cd71d59a9730ee2cb1b
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-01-04 12:47:46 +0000

    feat: add completions plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,9 +8,10 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "gopls" },
+				ensure_installed = { "lua_ls", "gopls" },
 			})
 		end,
 	},
@@ -19,15 +20,18 @@ return {
 		lazy = false,
 		config = function()
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
 				settings = { Lua = { diagnostics = { globals = { "vim" } } } },
 			})
 			lspconfig.gopls.setup({
+				capabilities = capabilities,
 				cmd = { "gopls" },
 				filetypes = { "go", "gomod", "gowork", "gotmpl" },
 				root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
