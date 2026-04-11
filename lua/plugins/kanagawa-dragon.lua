ab1018d1f48a1d16758dcc91699701dcbf022869
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2026-04-11 17:00:29 +0100

    chore: add gitignore; plugins changes

--- lua/plugins/kanagawa-dragon.lua
+++ lua/plugins/kanagawa-dragon.lua
@@ -9,5 +9,7 @@ return {
 			background = { dark = "dragon", light = "dragon" },
 		})
 		vim.cmd([[colorscheme kanagawa]])
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
