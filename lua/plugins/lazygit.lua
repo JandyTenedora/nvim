4d65b0c5ff754a283e4a8a194318a67775de38e8
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2026-04-19 16:13:57 +0000

    feat: add lazygit, diffview with branch diff and close keymaps

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,18 +1,25 @@
 return {
 	{
 		"kdheepak/lazygit.nvim",
+		lazy = false,
 		dependencies = { "nvim-lua/plenary.nvim" },
-		cmd = "LazyGit",
 		config = function()
 			vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
 		end,
 	},
 	{
 		"sindrets/diffview.nvim",
-		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+		lazy = false,
 		config = function()
 			vim.keymap.set("n", "<leader>dv", ":DiffviewOpen<CR>")
 			vim.keymap.set("n", "<leader>dh", ":DiffviewFileHistory<CR>")
+			vim.keymap.set("n", "<leader>dq", ":DiffviewClose<CR>")
+			vim.keymap.set("n", "<leader>dbv", function()
+				local branch = vim.fn.input("Branch to diff: ")
+				if branch ~= "" then
+					vim.cmd("DiffviewOpen " .. branch .. "...HEAD")
+				end
+			end)
 		end,
 	},
 }
